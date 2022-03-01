d7141877f0734628472d750465a880d6f7282281
Rory Schadler <roryschadler@gmail.com>
2022-03-01 17:44:53 -0500

    Initial project setup

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "node": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "react-app"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    },
+    "ignorePatterns": [
+        "**/node_modules/**"
+    ]
+}
