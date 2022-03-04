41c2ec0dca22d73d7ccc6ff4d55336ef695eb89d
Rory Schadler <roryschadler@gmail.com>
2022-03-03 22:38:58 -0500

    Documented Elevator Display

--- src/components/ElevatorDisplay.js
+++ src/components/ElevatorDisplay.js
@@ -2,16 +2,39 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Grid } from '@mui/material';
 
-import { CarPanel, FloorPanel } from './ElevatorPanel';
+import FloorPanel from './FloorPanel';
+import CarPanel from './CarPanel';
 
+/**
+ * Component for Tabbed Elevator Display
+ *
+ * Tab values should be passed into the `value` prop, and
+ * should be `'all'`, `'car'`, or a floor label. All
+ * available floors should be passed in the `floors` prop,
+ * and an `onClick` callback function is required for
+ * buttons to function properly.
+ *
+ * @component
+ * @example
+ * let tabValue = 'all';
+ * const floors = ['L', '1', '2'];
+ * const handleClick = () => console.log('click');
+ * return (
+ *   <ElevatorDisplay value={tabValue} floors={floors} onClick={handleClick} />
+ * );
+ */
 class ElevatorDisplay extends React.Component {
   static propTypes = {
+    /** Current tab value */
     value: PropTypes.string.isRequired,
+    /** List of all floors */
     floors: PropTypes.arrayOf(PropTypes.string).isRequired,
+    /** Callback on button click */
     onClick: PropTypes.func.isRequired,
   };
 
   render() {
+    // map floor labels to their corresponding button panels
     const floorButtons = Array.from(
       this.props.floors,
       (floor, index) => {
@@ -38,10 +61,16 @@ class ElevatorDisplay extends React.Component {
 
     if (this.props.value === 'all') { // full system display
       return (
-        <Grid container columns={{ xs: 4, sm: 8, md: 12 }} spacing={{ xs: 2, md: 3 }} alignItems='stretch'>
+        <Grid
+          container
+          columns={{ xs: 4, sm: 8, md: 12 }}
+          spacing={{ xs: 2, md: 3 }}
+          alignItems="stretch"
+        >
           <Grid item xs={2} sm={4} md={4}>
             {carPanel}
           </Grid>
+
           {floorButtons.map((button, index) => (
             <Grid item xs={1} sm={2} md={2} key={index}>
               {button}
