10dfe0a512f5c71cd3973abbaa7b41bf13ceb9dd
Rory Schadler <roryschadler@gmail.com>
2022-03-03 21:17:39 -0500

    Elevator Panels now wrapped in Paper
    Also implemented a proper onClick event handler so that state is handled
    correctly.

--- src/components/ElevatorPanel.js
+++ src/components/ElevatorPanel.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Grid from '@mui/material/Grid';
-import Stack from '@mui/material/Stack';
+import { Grid, Stack, Paper } from '@mui/material';
 
 import ElevatorButton from './ElevatorButton';
 
@@ -18,6 +17,7 @@ class ElevatorPanel extends React.Component {
     noText: PropTypes.bool,
     text: PropTypes.string,
     reverse: PropTypes.bool,
+    className: PropTypes.oneOfType([PropTypes.string, PropTypes.arrayOf(PropTypes.string)])
   };
 
   static defaultProps = {
@@ -51,6 +51,12 @@ class ElevatorPanel extends React.Component {
   }
 
   render() {
+    const className = ['elevator_panel'];
+    if (Array.isArray(this.props.className)) {
+      className.push(...this.props.className);
+    } else if (this.props.className) {
+      className.push(this.props.className);
+    }
     const textItem = !this.props.noText ? (
       <Grid item sx={{ textAlign: 'center' }}>
         {this.props.text}
@@ -58,18 +64,20 @@ class ElevatorPanel extends React.Component {
     ) : null;
 
     return (
-      <Grid container spacing={1} direction='column'>
-        {textItem}
-        <Grid
-          container
-          item
-          spacing={1}
-          alignItems='center'
-          justifyContent='center'
-        >
-          {this.splitColumns(this.props.children)}
+      <Paper className={className.join(' ')}>
+        <Grid container spacing={1} direction='column' justifyItems='center'>
+          {textItem}
+          <Grid
+            container
+            item
+            spacing={1}
+            alignItems='center'
+            justifyContent='center'
+          >
+            {this.splitColumns(this.props.children)}
+          </Grid>
         </Grid>
-      </Grid>
+      </Paper>
     );
   }
 }
@@ -79,6 +87,7 @@ class FloorPanel extends React.Component {
     floor: PropTypes.string.isRequired,
     location: PropTypes.string,
     noText: PropTypes.bool,
+    onClick: PropTypes.func.isRequired
   };
 
   static defaultProps = {
@@ -89,18 +98,19 @@ class FloorPanel extends React.Component {
     const panelProps = {
       noText: this.props.noText,
       text: `Floor ${this.props.floor}`,
+      className: 'floor_panel'
     };
     const upButton = (
       <ElevatorButton
         text='up'
-        onClick={buttonClick('up', this.props.floor)}
+        onClick={this.props.onClick('up', this.props.floor)}
         key='up'
       />
     );
     const downButton = (
       <ElevatorButton
         text='down'
-        onClick={buttonClick('down', this.props.floor)}
+        onClick={this.props.onClick('down', this.props.floor)}
         key='down'
       />
     );
@@ -128,24 +138,25 @@ class CarPanel extends React.Component {
   }
   static propTypes = {
     floors: PropTypes.arrayOf(PropTypes.string).isRequired,
+    onClick: PropTypes.func.isRequired
   };
 
   render() {
     const buttonList = this.props.floors.map((floor) => (
-      <ElevatorButton text={floor} onClick={buttonClick(floor)} key={floor} />
+      <ElevatorButton text={floor} onClick={this.props.onClick(floor)} key={floor} />
     ));
     return (
-      <ElevatorPanel text='Elevator Car' reverse>
+      <ElevatorPanel text='Elevator Car' reverse className='car_panel'>
         {buttonList}
       </ElevatorPanel>
     );
   }
 }
 
-function buttonClick(buttonText, floorString) {
-  return () =>
-    console.log(`${floorString ? floorString + ',' : ''}${buttonText}`);
-}
+// function buttonClick(buttonText, floorString) {
+//   return () =>
+//     console.log(`${floorString ? floorString + ',' : ''}${buttonText}`);
+// }
 
 export default ElevatorPanel;
 export { CarPanel, FloorPanel };
